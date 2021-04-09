3c266fa902d3f72f0c0806ad93e3bba837ee20c7
Aiz <dev@aiz.moe>
2021-04-09 15:37:11 +0200

    feat: replace layer numbers with enums

--- keymap.c
+++ keymap.c
@@ -1,8 +1,23 @@
 #include QMK_KEYBOARD_H
 
+
+enum niu_mini_layers {
+    _COLEMAK_DH,
+    _NUMBER,
+    _SYMBOL,
+    _NAVIGATION,
+    _FUNCTION,
+    _SETTING
+};
+
+#define NUMBER MO(_NUMBER)
+#define SYMBOL MO(_SYMBOL)
+#define NAVIGATION MO(_NAVIGATION)
+#define FUNCTION MO(_FUNCTION)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    /* Layer 0 Colemak
+    /* Colemak
      * ,-----------------------------------------------------------------------------------.
      * | Esc  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  |   `  |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -10,17 +25,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |  Tab |   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |  /   |Enter |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      | CTlr |  ALT | Shift|Layer1|    Space    |Layer2| GUI  | Ctlr |      |      |
+     * |      | CTlr |  ALT | Shift|Number|    Space    |  Nav | GUI  | Ctlr |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [0] = LAYOUT_planck_mit(
+    [_COLEMAK_DH] = LAYOUT_planck_mit(
         KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_GRV,
         KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,  KC_G,   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,    KC_QUOT,
         KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,   KC_K,   KC_H,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        XXXXXXX, KC_LCTL, KC_LALT, KC_LSFT, MO(1),     KC_SPC,     MO(3), KC_RGUI, KC_RCTL, KC_ALGR, XXXXXXX
+        XXXXXXX, KC_LCTL, KC_LALT, KC_LSFT, NUMBER,   KC_SPC, NAVIGATION, KC_RGUI, KC_RCTL, KC_ALGR, XXXXXXX
     ),
 
-    /* Layer 1 Numbers and Symbols
+    /* Numbers and Symbols
      * ,-----------------------------------------------------------------------------------.
      * |      |  !   |  @   |  #   |  $   |   %  |  =   |  7   |  8   |  9   |  +   |       |
      * |------+------+------+------+------+-------------+------+------+------+------+------ |
@@ -28,17 +43,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------ |
      * |      |  |   |   ]  |   }  |   )  |   &  |  _   |  1   |   2  |  3   |  \   |       |
      * |------+------+------+------+------+------+------+------+------+------+------+------ |
-     * |      |      |      |      |      |      0      |      |      |      |      |       |
+     * |      |      |      |      |      |      0      |Symbol|      |      |      |       |
      * `----------------------------------------------------------------------------------- '
      */
-    [1] = LAYOUT_planck_mit (
+    [_NUMBER] = LAYOUT_planck_mit (
         XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_RPRN, XXXXXXX ,
         XXXXXXX, KC_LSFT, KC_LBRC, KC_LCBR, KC_LPRN, KC_CIRC, KC_ASTR, KC_4,    KC_5,    KC_6,    KC_MINS, XXXXXXX ,
         XXXXXXX, KC_PIPE, KC_RBRC, KC_RCBR, KC_RPRN, KC_AMPR, KC_UNDS, KC_1,    KC_2,    KC_3,    KC_BSLS, XXXXXXX ,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       KC_0,       MO(2),   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       KC_0,      SYMBOL,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
-    /* Layer 2 Special Symbols
+    /* Special Symbols
      * ,-----------------------------------------------------------------------------------.
      * |      |  ¹   |  ²   |  ³   |   £  |  °   |      |      |      |      |  ¦   |       |
      * |------+------+------+------+------+-------------+------+------+------+------+------ |
@@ -49,14 +64,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      |      |      |      |      |             |      |      |      |      |       |
      * `----------------------------------------------------------------------------------- '
      */
-    [2] = LAYOUT_planck_mit (
+    [_SYMBOL] = LAYOUT_planck_mit (
         XXXXXXX, RSA(KC_1), RSA(KC_2),    RSA(KC_3),     ALGR(KC_4), XXXXXXX,       XXXXXXX,      XXXXXXX,   XXXXXXX,     XXXXXXX,    RSA(KC_BSLS),  XXXXXXX ,
         XXXXXXX, KC_LSFT,   RSA(KC_LBRC), ALGR(KC_LBRC), ALGR(KC_5), ALGR(KC_MINS), ALGR(KC_EQL), ALGR(KC_W), ALGR(KC_A), ALGR(KC_O), ALGR(KC_BSLS), XXXXXXX ,
         XXXXXXX, XXXXXXX,   RSA(KC_RBRC), ALGR(KC_RBRC), RSA(KC_4),  RSA(KC_MINS),  RSA(KC_EQL),  XXXXXXX,    XXXXXXX,    XXXXXXX,    RSA(KC_SLSH),  XXXXXXX ,
         XXXXXXX, XXXXXXX,   XXXXXXX,      XXXXXXX,       _______,              XXXXXXX,           _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,       XXXXXXX
     ),
 
-     /* Layer 3 Navigation and music
+     /* Navigation and Music
       * ,-----------------------------------------------------------------------------------.
       * |      |MStop | Mute | Vol- | Vol+ |MNext | Home | PGDN | PGUP | End  |      |      |
       * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -64,35 +79,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * |------+------+------+------+------+------|------+------+------+------+------+------|
       * |      | Undo | Cut  | Copy | Paste|MPrev |      |      |      |      |      |      |
       * |------+------+------+------+------+------+------+------+------+------+------+------|
-      * |      |      |      |      |Layer4|      |      |      |      |      |      |      |
+      * |      |      |      |      |Funct |      |      |      |      |      |      |      |
       * `-----------------------------------------------------------------------------------'
       */
-     [3] = LAYOUT_planck_mit(
+     [_NAVIGATION] = LAYOUT_planck_mit(
          XXXXXXX, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
          KC_DEL,  XXXXXXX, KC_LALT, KC_LSFT, KC_LCTL, KC_MPLY, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
          XXXXXXX, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_MPRV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-         XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT, MO(4),       _______,      KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+         XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT, FUNCTION,       _______,      KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
      ),
 
-      /* Layer 4 Function keys
-      * ,-----------------------------------------------------------------------------------.
-      * |      |      |      |      |      |      |      |  F1  |  F2  |  F3  |  F4  | TG5  |
-      * |------+------+------+------+------+-------------+------+------+------+------+------|
-      * |      | GUI  |  ALT |Shift | CTRL |      |      |  F5  |  F6  |  F7  |  F8  |      |
-      * |------+------+------+------+------+------|------+------+------+------+------+------|
-      * |      |      |      |      |      |      |      |  F9  |  F10 |  F11 |  F12 |      |
-      * |------+------+------+------+------+------+------+------+------+------+------+------|
-      * |      |      |      |      |      |      |      |      |      |      |      |      |
-      * `-----------------------------------------------------------------------------------'
+      /* Function keys
+      * ,------------------------------------------------------------------------------------.
+      * |      |      |      |      |      |      |      |  F1  |  F2  |  F3  |  F4  |Setting|
+      * |------+------+------+------+------+-------------+------+------+------+------+-------|
+      * |      | GUI  |  ALT |Shift | CTRL |      |      |  F5  |  F6  |  F7  |  F8  |       |
+      * |------+------+------+------+------+------|------+------+------+------+------+-------|
+      * |      |      |      |      |      |      |      |  F9  |  F10 |  F11 |  F12 |       |
+      * |------+------+------+------+------+------+------+------+------+------+------+-------|
+      * |      |      |      |      |      |      |      |      |      |      |      |       |
+      * `------------------------------------------------------------------------------------'
       */
-     [4] = LAYOUT_planck_mit(
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1, KC_F2,  KC_F3,  KC_F4,    TG(5),
+     [_FUNCTION] = LAYOUT_planck_mit(
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1, KC_F2,  KC_F3,  KC_F4,    TG(_SETTING),
          XXXXXXX, KC_LGUI, KC_LALT, KC_SLSH, KC_LCTL, XXXXXXX, XXXXXXX, KC_F5, KC_F6,  KC_F7,  KC_F8,    XXXXXXX,
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX,
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,     XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
      ),
 
-      /* Layer 5 Settings
+      /* Settings
       * ,-----------------------------------------------------------------------------------.
       * |Tgnkro|      |      |      |      |      |      | RMo_P| RMO_B| RMo_R|      |      |
       * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -103,8 +118,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * |      |      |      |      |      |      |      |      |      |      |      |      |
       * `-----------------------------------------------------------------------------------'
       */
-     [5] = LAYOUT_planck_mit(
-         NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_M_P, RGB_M_B, RGB_M_R, XXXXXXX, TG(5),
+     [_SETTING] = LAYOUT_planck_mit(
+         NK_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_M_P, RGB_M_B, RGB_M_R, XXXXXXX, TG(_SETTING),
          XXXXXXX, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX, RGB_M_SW,RGB_TOG, RGB_M_K, XXXXXXX, XXXXXXX,
          XXXXXXX, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX, RGB_M_X, RGB_M_SN,RGB_M_G, XXXXXXX, XXXXXXX,
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,     XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
